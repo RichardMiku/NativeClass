b97a8f87f87f3a36f445f4ff935ba98bd8bdd4d7
佳泽 李 <c4123986501@outlook.com>
2019-11-01 17:00:27 +0800

    NativeClass
    NC

---
+++ Native.Config/Config.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Native.Config
+{
+    public class Config
+    {
+        /// <summary>
+        /// WriteCfg
+        /// </summary>
+        /// <param name="FileName">文件名</param>
+        /// <param name="section">项名称</param>
+        /// <param name="term">节名称</param>
+        /// <param name="value">欲写值</param>
+        /// <returns></returns>
+        [DllImport("NativeConfig.dll")]
+        private static extern bool WriteCfg(string FileName, string section, string term, string value);
+        /// <summary>
+        /// ReadCfg
+        /// </summary>
+        /// <param name="FileName">文件名</param>
+        /// <param name="section">项名称</param>
+        /// <param name="term">节名称</param>
+        /// <returns></returns>
+        [DllImport("NativeConfig.dll")]
+        private static extern string ReadCfg(string FileName, string section, string term);
+        /// <summary>
+        /// 写配置项
+        /// </summary>
+        /// <param name="FileName">文件名</param>
+        /// <param name="section">项名称</param>
+        /// <param name="term">节名称</param>
+        /// <param name="value">欲写值</param>
+        /// <returns></returns>
+        public void WriteConfiguration(string FileName, string section, string term, string value)
+        {
+            bool W = WriteCfg(FileName, section, term, value);
+            if (W == false)
+            {
+                MessageBox.Show("写入配置项失败！", "提示");
+            }
+        }
+        /// <summary>
+        /// 读配置项
+        /// </summary>
+        /// <param name="FileName">文件名</param>
+        /// <param name="section">项名称</param>
+        /// <param name="term">节名称</param>
+        /// <returns></returns>
+        public string ReadConfiguration(string FileName, string section, string term)
+        {
+            string Cfg;
+            Cfg = ReadCfg(FileName, section, term);
+            return Cfg;
+        }
+    }
+}
---
+++ Native.Tools/Tools.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Native.Tools
+{
+    public class Tools
+    {
+        /// <summary>
+        /// HTTPReadFile
+        /// </summary>
+        /// <param name="http">http地址</param>
+        /// <returns></returns>
+        [DllImport("NativeTools.dll")]
+        private static extern byte HTTPReadFile(string http);
+        /// <summary>
+        /// HTTP读文件
+        /// </summary>
+        /// <param name="http">http地址</param>
+        /// <returns></returns>
+        public byte HttpReadFile(string http)
+        {
+            byte Value;
+            Value = HTTPReadFile(http);
+            return Value;
+        }
+        /// <summary>
+        /// 取运行目录
+        /// </summary>
+        /// <returns></returns>
+        public string GetRunningDirectory()
+        {
+            string dir;
+            dir = AppDomain.CurrentDomain.BaseDirectory;
+            return dir;
+        }
+        /// <summary>
+        /// LoadBall
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("NativeBall.dll")]
+        private static extern bool LoadBall();
+        /// <summary>
+        /// 加载悬浮球
+        /// </summary>
+        public void Loadball()
+        {
+            LoadBall();
+        }
+        /// <summary>
+        /// 检查网络连接check
+        /// </summary>
+        /// <param name="Description"></param>
+        /// <param name="ReservedValue"></param>
+        /// <returns></returns>
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(int Description, int ReservedValue);
+        /// <summary>
+        /// 检查网络连接
+        /// </summary>
+        /// <returns></returns>
+        public bool IsConnectInternet()
+          {
+              int Description = 0;
+             return InternetGetConnectedState(Description, 0);
+          }
+}
+}
